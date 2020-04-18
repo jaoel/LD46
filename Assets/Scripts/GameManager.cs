5fdde55574b58fea9425040d154998b1ec904ef4
Bentebent <henriksson.tim@gmail.com>
2020-04-18 09:37:19 +0200

    Queries hit trigger on

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private void Awake()
+    {
+        Physics.queriesHitTriggers = true;
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
