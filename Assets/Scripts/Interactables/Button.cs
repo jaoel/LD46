7ce3fb4cbed1ff1b6710bc2e5651875fa6c4d1e3
Bentebent <henriksson.tim@gmail.com>
2020-04-18 09:39:20 +0200

    Added interactable button

---
+++ Assets/Scripts/Interactables/Button.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ButtonState
+{
+    HOVER,
+    PRESSING,
+    RELEASED
+}
+
+public class Button : InteractableComponent
+{
+    private ButtonState _buttonState;
+
+    [SerializeField]
+    private float _pushSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (_buttonState)
+        {
+            case ButtonState.HOVER:
+                break;
+            case ButtonState.PRESSING:
+                UpdateState(State + _pushSpeed * Time.deltaTime);
+                break;
+            case ButtonState.RELEASED:
+                UpdateState(State - _pushSpeed * Time.deltaTime);
+                break;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _buttonState = ButtonState.PRESSING;
+        }
+        else
+        {
+            _buttonState = ButtonState.HOVER;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        _buttonState = ButtonState.RELEASED;
+    }
+}
--- Assets/Scripts/Interactables/InteractableComponent.cs
+++ Assets/Scripts/Interactables/InteractableComponent.cs
@@ -6,9 +6,19 @@ using UnityEngine.Events;
 public abstract class InteractableComponent : MonoBehaviour
 {
     //[0, 1]
-    protected float _state;
+    private float _state;
     public float State => _state;
 
     [SerializeField]
     UnityEventFloat _onStateChanged;
+
+    protected void Awake()
+    {
+        _state = 0.0f;
+    }
+
+    protected void UpdateState(float state)
+    {
+        _state = Mathf.Clamp(state, 0.0f, 1.0f);
+    }
 }
