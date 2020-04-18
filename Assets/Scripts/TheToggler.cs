ce262aebb24abf3973ae2ddd0d1f5fe786d74cd4
Jaoel <josv.svensson@gmail.com>
2020-04-18 11:16:47 +0200

    Add THE TOGGLER and lamp

---
+++ Assets/Scripts/TheToggler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TheToggler : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _onObjects = new List<GameObject>();
+    [SerializeField] private List<GameObject> _offObjects = new List<GameObject>();
+
+    public bool Toggled { get; private set; } = false;
+
+    public void Toggle(bool toggled) {
+        if (Toggled != toggled) {
+            foreach(GameObject go in _onObjects) {
+                go.SetActive(toggled);
+            }
+            foreach (GameObject go in _offObjects) {
+                go.SetActive(!toggled);
+            }
+        }
+        Toggled = toggled;
+    }
+}
