6baf6ada97be9646498eafff8dd9c7ff452e08c5
Jaoel <josv.svensson@gmail.com>
2020-04-18 11:56:37 +0200

    Add color control script

---
+++ Assets/Scripts/ColorControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ColorControl : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer _renderer = null;
+
+    private MaterialPropertyBlock mpb = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mpb = new MaterialPropertyBlock();
+    }
+
+    public void SetColor(Vector3 color) {
+        _renderer.GetPropertyBlock(mpb);
+        mpb.SetVector("_EmissionColor", color);
+        _renderer.SetPropertyBlock(mpb);
+    }
+}
