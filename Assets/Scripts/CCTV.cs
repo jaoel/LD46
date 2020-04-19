972849512d8140d9d38dcd640a25307a0ff3d599
Jaoel <josv.svensson@gmail.com>
2020-04-19 17:51:41 +0200

    CCTV and stuff

---
+++ Assets/Scripts/CCTV.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCTV : MonoBehaviour
+{
+    public GameObject pivot;
+
+    private void Update() {
+        pivot.transform.localRotation = Quaternion.Euler(0f, Mathf.Sin(Time.time * 0.5f) * 45f, 0f);
+    }
+}
