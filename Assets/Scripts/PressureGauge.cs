6ecc70ca9ab253e947ab586a4bcf953d7987e9ae
Jaoel <josv.svensson@gmail.com>
2020-04-19 09:24:18 +0200

    Add pressure gauge

---
+++ Assets/Scripts/PressureGauge.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PressureGauge : MonoBehaviour {
+    public GameObject needle;
+
+    public float State { get; private set; }
+
+    public void SetState(float newState) {
+        State = Mathf.Clamp01(newState);
+    }
+
+    private void Update() {
+        float vibratoState = Mathf.Clamp01((State - 0.7f) / 0.2f);
+        float vibrato = Mathf.Lerp(0f, Mathf.Sin(Time.time * 75f) * 5f, vibratoState);
+        needle.transform.localRotation = Quaternion.Euler(0f, 0f, Mathf.Lerp(-75f, 75f, State) + vibrato);
+    }
+}
