7636d4a1ede3c56bf080612839d82fcb230d23f1
Jaoel <josv.svensson@gmail.com>
2020-04-20 18:12:18 +0200

    Default audio at 50 percent

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -9,14 +9,14 @@ public class AudioManager : MonoBehaviour {
         private string _parameterName;
         private AudioMixer _audioMixer;
 
-        public float Volume { get; private set; } = 1f;
+        public float Volume { get; private set; } = 0.5f;
         public float Fade { get; private set; } = 1f;
 
         public MixerChannelVolume(string parameterName, AudioMixer audioMixer) {
             _parameterName = parameterName;
             _audioMixer = audioMixer;
 
-            Volume = PlayerPrefs.GetFloat("MixerChannelVolume" + _parameterName, 1f);
+            Volume = PlayerPrefs.GetFloat("MixerChannelVolume" + _parameterName, 0.5f);
             SetVolume(Volume);
         }
 
